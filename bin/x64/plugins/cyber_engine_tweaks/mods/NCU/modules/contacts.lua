2ae4565cc9b209a945b086e20dec0e3db58a0f8b
LucasAlcantara <LucasAlencarAlcantara@gmail.com>
2026-02-26 00:54:45 -0300

    feat: add contacts module defining main fixers and their details

--- bin/x64/plugins/cyber_engine_tweaks/mods/NCU/modules/contacts.lua
+++ bin/x64/plugins/cyber_engine_tweaks/mods/NCU/modules/contacts.lua
@@ -1,5 +1,5 @@
 -- contacts.lua
--- Gerencia os Dois Fixers principais do mod, ligações e liberação de missões
+-- Gerencia os Dois Fixers principais do mod (Definições)
 
 local Contacts = {
     car_fixer = {
@@ -18,38 +18,8 @@ local Contacts = {
 
 function Contacts:init()
     print("[NCU:Contacts] Módulo de Contatos inicializado.")
-    print("[NCU:Contacts] Fixer Ativo: " .. self.car_fixer.name .. " (" .. self.car_fixer.domain .. ")")
-    print("[NCU:Contacts] Fixer Ativo: " .. self.gang_fixer.name .. " (" .. self.gang_fixer.domain .. ")")
-    
-    -- Futuramente: Aqui escutaremos os eventos pra saber quando disparar as dublagens
-end
-
--- Simula verificação de missões do contato de Carros (Yelena)
-function Contacts:CheckCarContracts(trustLevel)
-    if trustLevel > 10 then
-        print("[NCU:" .. self.car_fixer.name .. "] Enviando contrato High-End: Hipercarro Corporativo.")
-    else
-        print("[NCU:" .. self.car_fixer.name .. "] Enviando contrato Inicial: Roube um sedã abandonado em Watson.")
-    end
-end
-
--- Simula verificação de missões do contato de Gangues (Ian)
-function Contacts:CheckGangContracts(trustLevel, territoryReputation)
-    if trustLevel > 10 and territoryReputation < -20 then
-        print("[NCU:" .. self.gang_fixer.name .. "] Maelstrom tá full pistola com você. Tenho um contrato pra limpar a barra, ou ir pra guerra.")
-    else
-        print("[NCU:" .. self.gang_fixer.name .. "] O básico. Vá em Heywood e dê um susto nuns devedores.")
-    end
-end
-
--- Essa função será responsável por acionar o áudio/interface no jogo depois
-function Contacts:TriggerHoloCall(contactId)
-    local fixer = self[contactId]
-    if fixer then
-        print(">> TOCA O TELEFONE <<")
-        print("[" .. fixer.name .. " (Holo)]: " .. fixer.greeting)
-        -- Aqui dentro vamos plugar o arquivo de som pt-br (.wem/.wav) via sistema do Cyberpunk
-    end
+    print("[NCU:Contacts] Fixer Ativo: " .. self.car_fixer.name)
+    print("[NCU:Contacts] Fixer Ativo: " .. self.gang_fixer.name)
 end
 
 return Contacts
